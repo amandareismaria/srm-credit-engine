3a91430f758098a414801adf7e52c4e45a770a49
Amanda Reis <amandareismaria@gmail.com>
2026-08-05 11:08:11 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/srm/casedev/CaseDevApplication.java
@@ -0,0 +1,13 @@
+package com.srm.casedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srm/casedev/CaseDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srm.casedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
