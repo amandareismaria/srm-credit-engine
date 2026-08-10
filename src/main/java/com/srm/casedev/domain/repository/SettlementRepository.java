b9b5e72c27a953c3840352f678959193a06bda74
Amanda Reis <amandareismaria@gmail.com>
2026-08-09 21:40:53 -0300

    fix: prevent duplicate receivable settlement

--- src/main/java/com/srm/casedev/domain/repository/SettlementRepository.java
+++ src/main/java/com/srm/casedev/domain/repository/SettlementRepository.java
@@ -4,4 +4,6 @@ import com.srm.casedev.domain.entity.Settlement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SettlementRepository extends JpaRepository<Settlement, Long> {
+
+    boolean existsByReceivableId(Long receivableId);
 }
